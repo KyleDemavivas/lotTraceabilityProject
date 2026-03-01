c1165f992176c1532bdb4e2b3c93f916d0a79716
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-02 06:54:29 +0800

    Added PHPUnit for future unit testing

---
+++ tests/SubmitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SubmitTest extends TestCase
+{
+    public function testSubmition(): void
+    {
+        $this->assertTrue(true);
+    }
+}
