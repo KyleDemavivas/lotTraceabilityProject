c08e863e6f1c69008f204e37e79672990cebed06
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-09 14:35:52 +0800

    added submitScrap function for submitting scrapped boards for recording in DB

--- js/boardFetchNoGood.js
+++ js/boardFetchNoGood.js
@@ -27,4 +27,25 @@ function getBoardData(data, source, onSuccess, onError) {
             }
         }
     });
+};
+
+function submitScrap(data, onSuccess, onError) {
+    $.ajax({
+        url: 'scrapSubmit.php',
+        data: data,
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        dataType: 'json',
+        success: function(response) {
+            if(typeof onSuccess === 'function') {
+                onSuccess(response);
+            }
+        },
+        error: function(xhr, status, error) {
+            if(typeof onError === 'function') {
+                onError(xhr, status, error);
+            }
+        }
+    })
 }
