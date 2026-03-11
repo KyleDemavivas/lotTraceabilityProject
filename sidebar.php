ecf9fada06630a486f1c62c9f057797f79ed5cbe
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-11 09:59:24 +0800

    added new favicon

--- sidebar.php
+++ sidebar.php
@@ -15,9 +15,11 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="/traceability/img/icon.jpg">
     <title>Production Traceability</title>
     <link rel="stylesheet" href="/traceability/css/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    
 </head>
 
 <body>
