a3f8f92d04921ec71d8d616c2f4170b5d7be7cce
moguraaa <demavivas.kyle.c@gmail.com>
2026-03-12 09:44:26 +0800

    changed the edit from using the handler to switching the HTML form instead as using the handler causes qty_input in data base to be the final_qtyinput causing a large size in other processes moving forward

--- mounter.php
+++ mounter.php
@@ -19,10 +19,10 @@ if (!isset($_SESSION['user_namefl'])) {
                     <label class="form-label">QR Code:</label>
                     <input type="text" class="form-input" name="qr_code" id="qr_code" autofocus autocomplete="off" minlength="21" maxlength="21" required>
                 </div>
-                <input type="text" class="form-input" name="final_qtyinput" id="final_qtyinput" readonly hidden>
+                <input type="text" class="form-input" name="qty_input" id="qty_input" readonly hidden>
                 <div class="form-group">
                     <label class="form-label">QTY INPUT:</label>
-                    <input type="text" class="form-input" name="qty_input" id="qty_input" readonly>
+                    <input type="text" class="form-input" name="final_qtyinput" id="final_qtyinput" readonly>
                 </div>
                 <div class="form-group">
                     <label class="form-label">OPERATOR:</label>
@@ -110,9 +110,8 @@ if (!isset($_SESSION['user_namefl'])) {
                             $('input[name="line"]').val(response.line);
                             $('input[name="shift"]').val(response.shift);
                             $('input[name="operator_name"]').val(response.operator_name);
-                            //CHANGE THIS TO SWITCH QTY INPUT AND FINAL QTY INPUT FIELDS
-                            $('input[name="final_qtyinput"]').val(parseInt(response.qty_input) || 0);
-                            $('input[name="qty_input"]').val(parseInt(response.final_qtyinput) || 0);
+                            $('input[name="qty_input"]').val(parseInt(response.qty_input) || 0);
+                            $('input[name="final_qtyinput"]').val(parseInt(response.final_qtyinput) || 0);
                             console.log(response.final_qtyinput);
 
                             let kepi_lot = response.kepi_lot;
