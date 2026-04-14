90ef8eba3cab7f76f21ee2f76e51741f9cc74e77
moguraaa <demavivas.kyle.c@gmail.com>
2026-04-14 08:31:00 +0800

    added update backend for repair submition to line leader verification

---
+++ repair_boardanalysis_submit.php
@@ -0,0 +1,34 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'].'/traceabilitydev/db_connect.ini';
+header('Content-Type: application/json;');
+
+$response = [
+    'success' => false,
+    'message' => 'Database Error.',
+    'data' => null,
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+        $serialcode = $_POST['serialcode'];
+
+        $query = "UPDATE repair_boardanalysis SET status = 'll' WHERE serialcode = :serialcode";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':serialcode', $serialcode);
+        $stmt->execute();
+        $response = [
+            'success' => true,
+            'message' => 'Board successfully sent for Line Leader Verification.',
+            'data' => null,
+        ];
+    } catch (Exception $e) {
+        $response = [
+            'success' => false,
+            'message' => $e,
+            'data' => null,
+        ];
+    }
+}
+
+echo json_encode($response);
