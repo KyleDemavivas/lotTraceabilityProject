ac4854d11dd7016cfa5f666dd33a3338df8101fd
moguraaa <demavivas.kyle.c@gmail.com>
2026-04-15 11:23:25 +0800

    logic for completed repair after modificator analysis for ICT boards

---
+++ mod_boardanalysis_submit.php
@@ -0,0 +1,51 @@
+<?php
+
+require $_SERVER['DOCUMENT_ROOT'].'/traceabilitydev/db_connect.ini';
+header('Content-Type: application/json;');
+
+$response = [
+    'success' => false,
+    'status' => 500,
+    'message' => 'Database Error.',
+    'data' => null,
+];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $serialcode = $_POST['serialcode'];
+
+    if (empty($serialcode)) {
+        throw new Exception('Serial Code is invalid!');
+        exit;
+    }
+
+    try {
+        $query = "UPDATE repair_boardanalysis SET status = 'done' WHERE serialcode = :serialcode";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':serialcode', $serialcode);
+        $stmt->execute();
+
+        $query = 'UPDATE trace_process SET wi_process=NULL, micro_process=NULL, partside1_process=NULL, partside2_process=NULL, fviss_process=NULL WHERE serialcode = :serialcode';
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':serialcode', $serialcode);
+        $stmt->execute();
+
+        $query = 'BEGIN TRANSACTION;
+                    DELETE FROM wi_process WHERE serialcode = :serialcode;
+                    DELETE FROM micro_process WHERE serialcode = :serialcode;
+                    DELETE FROM partside1_process WHERE serialcode = :serialcode;
+                    DELETE FROM partside2_process WHERE serialcode = :serialcode;
+                    DELETE FROM fviss_process WHERE serialcode = :serialcode;
+                    COMMIT;';
+        $stmt->bindParam(':serialcode', $serialcode);
+        $stmt->execute();
+    } catch (Exception $e) {
+        $response = [
+            'success' => false,
+            'status' => 400,
+            'message' => $e->getMessage(),
+            'data' => null,
+        ];
+    }
+}
+
+echo json_encode($response);
