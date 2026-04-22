0db4f12b05b8367d4ce00eb7719b96fbfc0bd89c
moguraaa <demavivas.kyle.c@gmail.com>
2026-04-22 08:27:52 +0800

    tweaked css for board history

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/jpg" href="/traceabilitydev/img/icon.jpg">
     <title>Production Traceability</title>
-    <link rel="stylesheet" href="/traceability/css/sidebar.css">
+    <link rel="stylesheet" href="/traceabilitydev/css/sidebar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     
 </head>
